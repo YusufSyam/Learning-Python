99a80644fa254d9b05bf79222b2ae3d4450f73be
Yusuf Syam <muhyusufsyamsyam@gmail.com>
2022-02-11 16:39:47 +0800

    Update taum_and_bday.py

--- HackerRank/taum_and_bday.py
+++ HackerRank/taum_and_bday.py
@@ -1,16 +1,48 @@
-b, w= 384, 887
-bc, wc, z= 2778, 6916, 7794
+#!/bin/python3
 
-total_gift= b+w
-both= (b*bc) + (w*wc)
+import math
+import os
+import random
+import re
+import sys
 
-def buy_one(total_gift, gift_to_buy, gift_price, gift_to_not_buy, rate):
-    buy_all= total_gift*gift_price
-    if(gift_to_buy==gift_to_not_buy):
-        total= (buy_all / total_gift) * gift_to_buy +  (gift_to_not_buy * (gift_price + rate))
-    else:
-        total= (buy_all/total_gift) * gift_to_buy + ((buy_all/total_gift) * gift_to_not_buy * (gift_price+rate))
+#
+# Complete the 'taumBday' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER b
+#  2. INTEGER w
+#  3. INTEGER bc
+#  4. INTEGER wc
+#  5. INTEGER z
+#
 
-    return int(total)
+def taumBday(b, w, bc, wc, z):
+    return b*min(bc, wc+z) + w*min(wc,bc+z)
 
-print((buy_one(total_gift, b, bc, w, z), buy_one(total_gift, w, wc, b, z), both))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
